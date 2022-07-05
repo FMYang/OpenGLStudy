c63000592c08651beeda989cd00f5aa18ed123cd
yangfangming <451493485@qq.com>
2022-07-05 18:29:57 +0800

    -

---
+++ LearnMetal/LearnMetal/Metal/Lession3(绘制三角形)/RenderTriangleVC.h
@@ -0,0 +1,16 @@
+//
+//  RenderTriangleVC.h
+//  LearnMetal
+//
+//  Created by yfm on 2022/7/5.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RenderTriangleVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
